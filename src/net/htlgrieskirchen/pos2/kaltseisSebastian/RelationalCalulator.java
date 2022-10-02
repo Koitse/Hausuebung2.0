451d8d281da30125a51b0c498b95ca09bcfc1b78
Koitse <kaltseisS190203@sus.htl-grieskirchen.at>
2022-10-02 15:02:49 +0200

    Calculator fertig

--- src/net/htlgrieskirchen/pos2/kaltseisSebastian/RelationalCalulator.java
+++ src/net/htlgrieskirchen/pos2/kaltseisSebastian/RelationalCalulator.java
@@ -1,29 +1,37 @@
 package net.htlgrieskirchen.pos2.kaltseisSebastian;
 
+import com.sun.source.tree.ReturnTree;
+
 public class RelationalCalulator extends AbstractCalculator{
 
 
-    public RelationalCalulator(CalculateOperation add, CalculateOperation subtract, CalculateOperation multiply, CalculateOperation divide) {
-        super(add, subtract, multiply, divide);
+    public RelationalCalulator() {
+        super((x,y) -> {
+            return new Number(x.getA()*y.getB()+(y.getA()*x.getB()), x.getB()*y.getB());
+        }, (x,y) -> {
+            return new Number(x.getA()*y.getB()-(y.getA()*x.getB()), x.getB()*y.getB());
+        }, (x,y) -> {
+            return new Number(x.getA()*y.getA(), x.getB()*y.getB());
+        }, (x,y) -> new Number(x.getA()*y.getB(), x.getB()*x.getA()));
     }
 
     @Override
-    public Number add(Number a, Number b) {
-        return null;
+    public Number add(Number x, Number y) {
+        return add.calc(x,y);
     }
 
     @Override
-    public Number subtract(Number a, Number b) {
-        return null;
+    public Number subtract(Number x, Number y) {
+        return subtract.calc(x,y);
     }
 
     @Override
-    public Number multiply(Number a, Number b) {
-        return null;
+    public Number multiply(Number x, Number y) {
+        return multiply.calc(x,y);
     }
 
     @Override
-    public Number divide(Number a, Number b) {
-        return null;
+    public Number divide(Number x, Number y) {
+        return divide.calc(x,y);
     }
 }
